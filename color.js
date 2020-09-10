ae0360f4284e14087dddfd7c01fe246b815cb033
QuietMisdreavus <victoria@quietmisdreavus.net>
2020-09-10 17:47:29 -0600

    add fold column

--- color.js
+++ color.js
@@ -177,7 +177,7 @@ function addBgColor(colors, name, useAnsi, limit, bgDistinct) {
     }
 }
 
-var uiColorNames = ['linenr', 'statusline', 'tabline', 'tablinesel', 'folded'];
+var uiColorNames = ['linenr', 'statusline', 'tabline', 'tablinesel', 'folded', 'foldcolumn'];
 
 function addUiColor(colors, name, useAnsi, limit, uiFrameValid) {
     let bgName = `${name}-bg`;
